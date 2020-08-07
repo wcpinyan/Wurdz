73f6a170d7cc06d0af4dde8b1a077738e54a40e9
wcpinyan <wcpinyan@gmail.com>
2020-08-07 15:29:22 -0500

    Add project files.

---
+++ Shared/Models/GameBoard.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Wurdz.Shared.Models
+{
+    public class GameBoard
+    {
+        public GameBoard()
+        {
+            
+        }
+        public GameBoard(int _numCellsPerSide)
+        {
+            int count = 0; 
+            numCellsPerSide = _numCellsPerSide;
+            title = "Hello Wurdz";
+            cells = new Cell[numCellsPerSide,numCellsPerSide];
+           // Random rand = new Random();
+            for (int i = 0; i < numCellsPerSide ; i++){
+                for (int j = 0; j < numCellsPerSide; j++)
+                {
+                    //int gen = rand.Next(1, numCellsPerSide); //gen=random generated #
+                    Cell cell = new Cell();
+                    cell.row = i;
+                    cell.col = j;
+                    if(i==7 && j == 7)
+                    {
+                        cell.color = "white";
+                        cell.caption = "X";
+                    }                   
+                    
+                    
+                    count++;
+                    SetColor(cell);
+                    cells[i, j] = cell;
+                    Console.WriteLine(i + "," + j + cell.color);
+                }
+                
+                
+            }
+            CreatePinkCrissCross();
+            void CreatePinkCrissCross()
+            {
+                int rowCount = 0;
+                
+                foreach (var c in cells)
+                {
+                    if (c.row == rowCount)
+                    {
+                        if (c.color == "white") //other primary color has not been placed in this square, so give it pink if appropriate
+                        {
+                            if(c.col==7 && c.row == 7) //this is white center piece so skip
+                            {
+                                continue;
+                            }
+                            if (c.col ==rowCount || c.col == numCellsPerSide - rowCount-1)
+                            {
+                                c.color = "pink";
+                                c.caption = "DW";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        rowCount++;
+                    }
+
+                    
+                }
+                        
+                    
+
+               
+            }
+            void SetColor(Cell c)
+            {
+                // setup a criss-cross of pink squares...then override with other colors as necessary
+               
+                //set center square to white 1st since otherwise it would be set to red
+                if ( c.row == 7 &&  c.col == 7)
+                {
+                    c.caption = "X";
+                    c.color = "white";
+                    return;
+                }
+                if ((c.row==0 || c.row==7 || c.row==14) && (c.col==0 || c.col==7 || c.col == 14))
+                {
+                    c.caption = "TW";
+                    c.color = "red";
+                    return;
+                }
+                if ((c.row == 0 && (c.col==3 || c.col==11)) || 
+                    (c.row == 2 && (c.col == 6 || c.col == 8)) ||
+                   ( c.row == 3 && (c.col == 0 || c.col == 7 || c.col == 14)))
+                   /* ||
+                   (c.row == 3 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 6 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 7 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 8 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 11 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 12 && (c.col == 3 || c.col == 11)) ||
+                   (c.row == 14 && (c.col == 3 || c.col == 11))) */
+                {
+                    c.caption = "DL";
+                    c.color = "lightblue";
+                    return;
+                }
+                 else   
+                {
+                    c.caption = "";
+                    c.color = "white";
+                    return;
+                }
+                if ((c.row == 0 || c.row == 7 || c.row == 14) && (c.col == 0 || c.col == 7 || c.col == 14))
+                {
+                    c.caption = "DW";
+                    c.color = "red";
+                    return;
+                }
+            }
+
+        }
+        public int id { get; set; }
+        public string title { get; set; }
+        public int numCellsPerSide { get; set; }
+        public Cell[,] cells { get; set; }
+
+       
+    }
+    public class Cell
+    {
+        public int id { get; set; }
+        public int width { get; set; }
+        public string caption { get; set; }
+        public int col { get; set; }
+        public int row { get; set; }
+        public string color { get; set; }
+        public  int multiplyer { get; set; }
+    }
+}
---
+++ Shared/Models/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/* TILE images downloaded from https://suncatcherstudio.com/image-editor/ */
+
+namespace Wurdz.Shared.Models
+{
+    public class Tile
+    {
+        public int id { get; set; }
+        public string tileImage { get; set; }
+        public string letter { get; set; }
+        public int pointValue { get; set; }
+        public int dropCol { get; set; }
+        public int dropRow { get; set; }
+
+        public Tile[,] tilePool { get; set; }
+        public Tile[,] player1Tiles { get; set; }
+        public Tile[,] player2Tiles { get; set; }
+        public Tile[,] player3Tiles { get; set; }
+        public Tile[,] player4Tiles { get; set; }
+
+        void BuildTilePool()
+        {
+
+        }
+    }
+}
