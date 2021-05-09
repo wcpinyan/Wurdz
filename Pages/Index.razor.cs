0d42deeb785f8fe4155656e7701fe3064ada304a
wcpinyan <wcpinyan@gmail.com>
2021-05-08 20:22:28 -0500

    Added drag-drop options

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wurdz.Shared.GameManager;
+using Wurdz.Shared.Models;
+
+namespace Wurdz.Pages
+{
+    public partial class Index
+    {
+        void ProcessState(GameManager.GameState state)
+        {
+             switch (state)
+            {
+                
+                case GameManager.GameState.BoardConstructed:
+                    createTilePool();
+                    break;
+                case GameManager.GameState.PlayersSelected:
+                    break;
+            
+            }
+        }
+        async Task UpdatePlayerHand(TileModel t)
+        {
+            currentWord.Add(t);
+            appState.player1Tiles.Remove(t);
+            await OnUpdate.InvokeAsync(null);
+        }
+        void createTilePool()
+        {
+            try
+            {
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    string url = $"images/{letters[i]}.svg";
+             
+                    ti = new TileModel(i, url, letters[i].ToString(), tilenums[i, 1], tilenums[i, 0]);
+                    tilePool = TileModel.tilePool; //tilePool is a static class variable not instance var
+                                                   // Console.WriteLine(tilePool);
+                    shuffled = TileModel.ShufflePool();
+                    appState.shuffledTiles = shuffled;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR:  ======>" + ex.Message.ToString());
+            }
+
+
+        }
+        void ScoreWord()
+        {
+            Cell cell = null;
+            int wordMultiplyer = 1;
+
+            foreach (TileModel t in currentWord)
+            {
+                cell = t.cellDroppedOn;
+                //assume 1 for all then update for anything above
+                if (cell.wordMultiplyer > 1)
+                {
+                    wordMultiplyer = cell.wordMultiplyer;
+                }
+                wordScore += t.pointValue * cell.letterMultiplier;
+            }
+            wordScore *= wordMultiplyer;
+        }
+        void RecallWord()
+        {
+            Cell cell;
+
+            foreach (TileModel t in currentWord)
+            {
+
+                cell = t.cellDroppedOn;
+                if (cell.col == 7 && cell.row == 7)
+                {
+                    cell.image = "images/star.png";
+                }
+                else
+                {
+                    cell.image = "";
+                }
+                appState.player1Tiles.Add(t);
+
+            }
+            currentWord.Clear();
+            StateHasChanged();
+        }
+    }
+}
---
+++ Shared/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wurdz.Shared.GameManager
+{
+    public class GameManager
+    {
+        public GameManager()
+        {
+
+        }
+        public enum GameState
+        {
+            BoardConstructed,
+            PlayersSelected,
+            GameStarted,
+            HandsDealt,
+            NextPlayerSelected,
+            TilePlayed,
+            WordBuilt,
+            WordVerified,
+            WordScored,
+            WordChallenged,
+            WordValidated,
+            WordReset,
+            ReadyForNextPlayer,
+            TilePoolEmpty,
+            HandHasNoWord,
+            NoMorePlays,
+            GameOver,
+            WinnerDeclared,
+            WinnerAddedToBoard
+        }
+        public enum Action
+        {
+            Invite,
+            Start,
+            Deal,
+            SelectNextPlayer,
+            Draw,
+            DragLetter,
+            EndWordConstruction,
+            ScoreWord,
+            RecallWord,
+            ValidateWord,
+            Challenge,
+            Pass,
+            Stop,
+            DisplayGameStats,
+            DisplayWinnerBoard
+        }
+        public GameState ChangeState(GameState current, Action action) =>
+            (current, action) switch
+            {
+                (GameState.BoardConstructed, Action.Invite) => GameState.PlayersSelected,
+                (GameState.PlayersSelected, Action.Start) => GameState.GameStarted,
+                (GameState.GameStarted, Action.Deal) => GameState.HandsDealt,
+                (GameState.HandsDealt, Action.SelectNextPlayer) => GameState.PlayersSelected,
+                (GameState.PlayersSelected, Action.DragLetter) => GameState.TilePlayed,
+                (GameState.TilePlayed, Action.EndWordConstruction) => GameState.WordBuilt,
+                (GameState.WordBuilt, Action.ScoreWord) => GameState.WordScored,
+                (GameState.WordScored, Action.Challenge) => GameState.WordChallenged,
+                (GameState.WordChallenged, Action.RecallWord) => GameState.WordReset,
+                (GameState.WordChallenged, Action.ValidateWord) => GameState.ReadyForNextPlayer,
+                (GameState.WordScored, Action.Draw) => GameState.ReadyForNextPlayer,
+                (GameState.ReadyForNextPlayer, Action.SelectNextPlayer) => GameState.PlayersSelected,
+                (GameState.TilePoolEmpty, Action.Draw) => GameState.ReadyForNextPlayer,
+                (GameState.HandHasNoWord, Action.Pass) => GameState.ReadyForNextPlayer,
+                (GameState.NoMorePlays, Action.Stop) => GameState.GameOver,
+                (GameState.GameOver, Action.DisplayGameStats) => GameState.WinnerDeclared,
+                (GameState.WinnerDeclared, Action.DisplayWinnerBoard) => GameState.WinnerAddedToBoard,
+                _ => throw new NotSupportedException(
+            $"{current} has no transition on {action}")
+            };
+    }
+}
